c562bf75bdbb587c49b3fcc8a592e0a2e8ef2cce
Ankit <ankitsolanki2608@gmail.com>
2021-06-26 01:39:04 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './components/Board';
+import { calculateWinner } from './helper';
 
 import './styles/root.scss';
 
 const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isNext, setisNext] = useState(false);
+
+  const winner = calculateWinner(board);
+  const massage = winner
+    ? `Winner is ${winner}`
+    : `Next Player is ${isNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+    if (board[position] || winner) {
+      return;
+    }
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+    });
+    setisNext(prev => !prev);
+  };
   return (
     <div className="app">
       <h1>Tic Tac Toe</h1>
-      <Board />
+      <h2>{massage}</h2>
+      <Board board={board} handleSquareClick={handleSquareClick} />
     </div>
   );
 };
